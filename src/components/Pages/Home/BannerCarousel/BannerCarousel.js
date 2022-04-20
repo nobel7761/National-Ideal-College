3ff72b37f64d4aecd100a86512fcad6142ce0346
mynul <homeexercise8@gmail.com>
2022-04-21 00:51:02 +0600

    carousel add

--- src/components/Pages/Home/BannerCarousel/BannerCarousel.js
+++ src/components/Pages/Home/BannerCarousel/BannerCarousel.js
@@ -1,29 +1,31 @@
-import React, { useState } from 'react';
-import './BannerCarousel.css';
+import React from 'react';
 import { Carousel } from 'react-bootstrap';
 import Image1 from '../../../../images/carousel/1.jpg';
 import Image2 from '../../../../images/carousel/2.jpg';
 import Image3 from '../../../../images/carousel/3.jpg';
-
+import icon1 from '../../../../images/h-ic1.png';
+import icon2 from '../../../../images/h-ic2.png';
+import icon3 from '../../../../images/h-ic3.png';
+import icon4 from '../../../../images/h-ic4.png';
+import './BannerCarousel.css';
 const BannerCarousel = () => {
-    const [index, setIndex] = useState(0);
 
-    const handleSelect = (selectedIndex, e) => {
-        setIndex(selectedIndex);
-    };
 
     return (
-        <Carousel activeIndex={index} onSelect={handleSelect} className='carousel-container'>
-            <Carousel.Item>
+        <div>    <Carousel indicators={false} interval={3000} >
+            <Carousel.Item >
                 <img
                     className="d-block w-100"
                     src={Image1}
                     alt="First slide"
                 />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
+                <div className="caption">
+                    <div>                    <h2 className="tittle"> WELCOME TO <span className="tittleLastPart">UNIVERSITY</span></h2>
+                        <p className="description">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form</p>
+                        <button className=" buttons">ALL COURSES</button>
+                        <button className="ms-5 buttons buttons2">READ MORE</button></div>
+
+                </div>
             </Carousel.Item>
             <Carousel.Item>
                 <img
@@ -32,10 +34,14 @@ const BannerCarousel = () => {
                     alt="Second slide"
                 />
 
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
+                <div className="caption">
+
+
+                    <h2 className="tittle">ADMISSION OPEN <span className="tittleLastPart">2018</span></h2>
+                    <p className="description">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form</p>
+                    <button className=" buttons">ADMISSION</button>
+                    <button className="ms-5 buttons buttons2">READ MORE</button>
+                </div>
             </Carousel.Item>
             <Carousel.Item>
                 <img
@@ -44,14 +50,36 @@ const BannerCarousel = () => {
                     alt="Third slide"
                 />
 
-                <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                <div className="caption">
+
+
+                    <h2 className="tittle">EDUCATION <span className="tittleLastPart">MASTER</span></h2>
+                    <p className="description">
+                        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form
                     </p>
-                </Carousel.Caption>
+                    <button className=" buttons">ALL COURSES</button>
+                    <button className="ms-5 buttons buttons2">READ MORE</button>
+                </div>
+
             </Carousel.Item>
         </Carousel>
+                <div className="d-flex w-25 mx-auto gx-2 fixed_item">
+                    <div className=" icon-bg-width">
+                        <img src={icon1} alt="" /> <br />
+                    <p className="text-white">ACADEMY</p>
+                    </div>
+                    <div className=" icon-bg-width"><img src={icon2} alt="" />
+                    <p className="text-white">ADMISSION</p>
+                    </div>
+                    <div className=" icon-bg-width"><img src={icon3} alt="" />
+                    <p className="text-white">COURSES</p>
+                    </div>
+                    <div className=" icon-bg-width"><img src={icon4} alt="" />
+                    <p className="text-white">SEMINER</p></div>
+                </div>
+
+        </div>
+
     );
 };
 
